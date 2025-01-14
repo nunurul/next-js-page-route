ea420654a61d1697273869ef2bdbece4f7ba8a9a
nunurul <noe.rul.khoir@gmail.com>
2025-01-14 10:28:04 +0700

    with firebase

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,16 +1,41 @@
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 
-const Product = () => {
-  // const [isLogin, setIsLogin] = useState(false);
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
 
-  // const { push } = useRouter();
-  // useEffect(() => {
-  //   if (!isLogin) {
-  //     push("/auth/login");
-  //   }
-  // }, []);
-  return <div>Product</div>;
+const Product = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState<Data["data"]>([]);
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (!isLogin) {
+      push("/auth/login");
+    }
+  }, []);
+
+  useEffect(() => {
+    fetch("/api/product")
+      .then((res) => res.json())
+      .then((response) => {
+        setProducts(response.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Product Page</h1>
+      {products.map((product: productType) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Product;
